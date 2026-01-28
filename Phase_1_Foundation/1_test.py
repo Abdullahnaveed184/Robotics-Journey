0f27272cebb434c6e7639edf9c4107c560e13363
Abdullahnaveed184 <abdullahnaveed662009@gmail.com>
2026-01-28 19:12:48 +0500

    Update and rename day1_test.py to 1_test.py

---
+++ Phase_1_Foundation/1_test.py
@@ -0,0 +1,6 @@
+This is my basic introduion to the words most basic coding language, python.
+--It is easy and simple to use and i learnt in no time.
+print("Hello Robotics")
+print("Robot is alive")
+--So this is the starting of the code.
+
--- Phase_1_Foundation/day1_test.py
+++
@@ -1,3 +0,0 @@
-print("Hello Robotics")
-print("Robot is alive")
-
