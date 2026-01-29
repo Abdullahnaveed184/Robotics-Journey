8e195290815eb0f33188eedc60392d02b2f316ac
Abdullahnaveed184 <abdullahnaveed662009@gmail.com>
2026-01-29 17:54:02 +0500

    Update day4_led_arduino.ino

--- Phase_1_Foundation/day4_led_arduino.ino
+++ Phase_1_Foundation/day4_led_arduino.ino
@@ -1,3 +1,4 @@
+LED working model:-
 void setup() {
 pinMode(13, OUTPUT);
 }
@@ -10,3 +11,8 @@ digitalWrite(13, LOW);
 delay(1000);
 
 }
+--These simple magical words act as a working command of the LED.
+--LED need this code to get power and blink.
+--Without this the LED will be as boring as you are while reading other students portfolio.
+--But with this it will be fascinating as it is while reading my portfolio.
+  
