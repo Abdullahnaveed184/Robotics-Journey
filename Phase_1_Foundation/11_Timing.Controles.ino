c6f3e139f56e076e435f19baed174df239ddee23
Abdullahnaveed184 <abdullahnaveed662009@gmail.com>
2026-02-05 15:22:32 +0500

    Add files via upload

---
+++ Phase_1_Foundation/11_Timing.Controles.ino
@@ -0,0 +1,27 @@
+Time dealation and time counting:-
+--We already know about the delay(1000).
+--It means Arduino will do nothing for 1 sec.
+--But this code will fix it:
+const int ledPin = 13;
+
+unsigned long previousTime = 0;
+const int interval = 1000;
+
+bool ledState = false;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentTime = millis();
+
+  if (currentTime - previousTime >= interval) {
+    previousTime = currentTime;
+
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);
+  }
+}
+--unsigned long will record time as controller starts.
+--Timing help a lot in robot moving, turning and performing precise work.
